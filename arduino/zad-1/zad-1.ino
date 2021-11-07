d45c7d974145556d59c78a0fe598dd88a6cebb59
Paulina <pmaciejewska20@gmail.com>
2021-11-07 13:18:16 +0100

    Flashing LED while the button is pressed

--- arduino/zad-1/zad-1.ino
+++ arduino/zad-1/zad-1.ino
@@ -1,46 +1,25 @@
-//MIGANIE DIOD(DWOCH) JAK NA SWIATLACH
+//FLASHING LED, WHILE THE BUTTON IS PRESSED
 
 void setup()
 {
-  pinMode(9, OUTPUT);   //dioda zolta
-  pinMode(10, OUTPUT);    //dioda czerwona
+  pinMode(10, OUTPUT);   // yellow LED
+  pinMode(9, OUTPUT);    // red LED
 
-  pinMode(7, INPUT_PULLUP);   //przycisk
+  pinMode(7, INPUT_PULLUP);   // button
   
-  digitalWrite(9, LOW);   //wylaczenie diod
-  digitalWrite(10, LOW);
+  digitalWrite(10, LOW);   // LEDs off
+  digitalWrite(9, LOW);
 }
 
 void loop() 
 {
-
-  if(digitalRead(7) == LOW) 
-  {
-    
-    digitalWrite(9, HIGH);
-    delay(1000);
-
-    digitalWrite(10, HIGH);
-    delay(1000);
-
-    digitalWrite(9, LOW);
-    delay(2000);
-
-    digitalWrite(10, LOW);
-    delay(3000);
-    
-
-    digitalWrite(10, HIGH);
-    delay(1000);
-
-    digitalWrite(10, LOW);
-
-    digitalWrite(9, HIGH);
-    delay(1000);
-
-    digitalWrite(9, LOW);
- 
-  }
   
+  while (digitalRead(7) == LOW)     // If the button is pressed
+  {
+    digitalWrite(10, LOW);        // red LED off
+    delay(1000);
+    digitalWrite(10, HIGH);        // red LED on
+    delay(1000);
+  }
 
 }
