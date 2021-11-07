4325b3deb60d56bfea7c79dfdc1c294cae007a78
Paulina <pmaciejewska20@gmail.com>
2021-11-07 20:52:18 +0100

    LED + annoucement

--- arduino/zad-2/zad-2.ino
+++ arduino/zad-2/zad-2.ino
@@ -1,14 +1,32 @@
-//UART - SENDING TEXT TO COMPUTER
+//UART - LEDs + ANNOUCEMENT
 
 void setup()
 {
   Serial.begin(9600);   // Setting the baud rate
-  Serial.println("Welcome in Forbot!"); //One-time text sending
+  
+  pinMode(9, OUTPUT);   //red LED
+  pinMode(10, OUTPUT);    //green LED
+  pinMode(7,INPUT_PULLUP);    //button
+
+  digitalWrite(9, LOW); //Both LEDs off
+  digitalWrite(10, LOW);
 }
 
 void loop() 
 {
- delay(5000);
- Serial.println("5 seconds have passed!");    //Sending in loop
-
+  if(digitalRead(7) == LOW)
+  {
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
+    Serial.println("The window is OPEN!!!");
+    delay(5000);
+  }
+  
+  else
+  {
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    Serial.println("ATTENTION! ALARM! The window is closed");
+    delay(5000);
+  }
 }
