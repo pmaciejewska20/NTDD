e2bf55ed6975422bce02c94cac2deeab23087bc8
Paulina <pmaciejewska20@gmail.com>
2021-11-08 00:54:03 +0100

    counter

--- arduino/zad-2/zad-2.ino
+++ arduino/zad-2/zad-2.ino
@@ -1,40 +1,14 @@
-//UART - LEDs + ANNOUCEMENT + #define directive
-#define buttonPin 7
-#define ledPinRed 9
-#define ledPinGreen 10 
+//COUNTER
+int licznik = 0;
 
 void setup()
 {
-  Serial.begin(9600);   // Setting the baud rate
-  
-  pinMode(ledPinRed, OUTPUT);   //red LED
-  pinMode(ledPinGreen, OUTPUT);    //green LED
-  pinMode(buttonPin,INPUT_PULLUP);    //button
-
-  digitalWrite(ledPinRed, LOW); //Both LEDs off
-  digitalWrite(ledPinGreen, LOW);
+  Serial.begin(9600);
 }
 
 void loop() 
 {
-  if(digitalRead(buttonPin) == LOW) //if button is pressed
-  {
-    digitalWrite(ledPinGreen, HIGH);   //green LED on
-    digitalWrite(ledPinRed, LOW);   //red LED off
-    Serial.println("The window is OPEN!!!");    //annoucement
-    delay(500);
-  }
-  
-  else                      // if button isn't pressed
-  {
-    digitalWrite(ledPinGreen, LOW);    //green LED on
-    digitalWrite(ledPinRed, HIGH);    //red LED off
-    Serial.println("ATTENTION! ALARM! The window is closed");   //annoucement
-    delay(100);
-
-    while(digitalRead(buttonPin) == HIGH)
-    {
-      delay(30);
-    }
-  }
+  Serial.println(licznik);
+  licznik++;
+  delay(100);
 }
