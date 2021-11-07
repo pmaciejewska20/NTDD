d4c2efdfe214855734fe195c5d45a9c44a877892
Paulina <pmaciejewska20@gmail.com>
2021-11-07 22:03:15 +0100

    #define directive added

--- arduino/zad-2/zad-2.ino
+++ arduino/zad-2/zad-2.ino
@@ -1,35 +1,38 @@
-//UART - LEDs + ANNOUCEMENT
+//UART - LEDs + ANNOUCEMENT + #define directive
+#define buttonPin 7
+#define ledPinRed 9
+#define ledPinGreen 10 
 
 void setup()
 {
   Serial.begin(9600);   // Setting the baud rate
   
-  pinMode(9, OUTPUT);   //red LED
-  pinMode(10, OUTPUT);    //green LED
-  pinMode(7,INPUT_PULLUP);    //button
+  pinMode(ledPinRed, OUTPUT);   //red LED
+  pinMode(ledPinGreen, OUTPUT);    //green LED
+  pinMode(buttonPin,INPUT_PULLUP);    //button
 
-  digitalWrite(9, LOW); //Both LEDs off
-  digitalWrite(10, LOW);
+  digitalWrite(ledPinRed, LOW); //Both LEDs off
+  digitalWrite(ledPinGreen, LOW);
 }
 
 void loop() 
 {
-  if(digitalRead(7) == LOW) //if button is pressed
+  if(digitalRead(buttonPin) == LOW) //if button is pressed
   {
-    digitalWrite(10, HIGH);   //green LED on
-    digitalWrite(9, LOW);   //red LED off
+    digitalWrite(ledPinGreen, HIGH);   //green LED on
+    digitalWrite(ledPinRed, LOW);   //red LED off
     Serial.println("The window is OPEN!!!");    //annoucement
     delay(500);
   }
   
   else                      // if button isn't pressed
   {
-    digitalWrite(10, LOW);    //green LED on
-    digitalWrite(9, HIGH);    //red LED off
+    digitalWrite(ledPinGreen, LOW);    //green LED on
+    digitalWrite(ledPinRed, HIGH);    //red LED off
     Serial.println("ATTENTION! ALARM! The window is closed");   //annoucement
     delay(100);
 
-    while(digitalRead(10) == HIGH)
+    while(digitalRead(buttonPin) == HIGH)
     {
       delay(30);
     }
