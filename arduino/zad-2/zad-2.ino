1371c1f51adca8826ed169eca1a71e26be3ec70f
Paulina <pmaciejewska20@gmail.com>
2021-11-08 01:42:52 +0100

    LEDs control by UART

--- arduino/zad-2/zad-2.ino
+++ arduino/zad-2/zad-2.ino
@@ -1,10 +1,18 @@
-//BIDIRECTIONAL TRANSMISSION
+//LEDs control by UART (Universal Asynchronous Receiver/Transmitter)
+
+#define pinLedGreen 10
+#define pinLedRed 9
 
 String odebraneDane = ""; //Empty string of read characters
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(pinLedRed, OUTPUT);
+  pinMode(pinLedGreen, OUTPUT);
+
+  digitalWrite(pinLedRed, LOW);
+  digitalWrite(pinLedGreen, LOW);
 }
 
 void loop() 
@@ -12,6 +20,19 @@ void loop()
   if (Serial.available() > 0)
   {
     odebraneDane = Serial.readStringUntil('\n');
-    Serial.println("Hello " + odebraneDane + "!"); //show the message
+    
+    if (odebraneDane == "pinLedGreen")
+    {
+      digitalWrite(pinLedGreen, HIGH);
+      delay(1000);
+      digitalWrite(pinLedGreen, LOW);
+    }
+    
+    if (odebraneDane == "pinLedRed")
+    {
+      digitalWrite(pinLedRed, HIGH);
+      delay(1000);
+      digitalWrite(pinLedRed, LOW);
+    }
   }
 }
