644680b0b1f7dbd5fc9cfb9700391fcc78b457b0
Paulina <pmaciejewska20@gmail.com>
2021-11-12 11:09:19 +0100

    simple game

--- arduino/zad-3./zad-3..ino
+++ arduino/zad-3./zad-3..ino
@@ -1,26 +1,82 @@
-//LED shining in the dark V2(potentiometer added - smooth activation threshold)
- 
+//simple game
+
+#define ledGreen 8
+#define ledYellow 9
+#define ledRed 10
+#define button 11
+
+int enteredNumberInt = 0;
+String enteredNumber = "";
 int readValue = 0; 
-int threshold = 0;
+byte tries = 2;
+boolean startAgain = true;
+
 
 void setup() 
 {
-  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+  
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  
+  pinMode(button, INPUT_PULLUP); //button
+
+  digitalWrite(ledGreen, LOW);
+  digitalWrite(ledYellow, LOW);
+  digitalWrite(ledRed, LOW);
 }
 
 void loop() 
 {
-  readValue = analogRead(A5);
-  threshold = analogRead(A4);
-  
-  if (readValue < threshold)
+  if (startAgain == true)
   {
-      digitalWrite(8,HIGH);
-  }
-  
-  else
-  {
-    digitalWrite(8, LOW);
+    Serial.println("Turn the potentiometer and press the button. ");
+    
+    while (digitalRead(button) == LOW) delay(25);
+    while (digitalRead(button) == HIGH) delay(250);
+    readValue = analogRead(A5);
+    Serial.println(readValue);
+    Serial.println("Enter the number between 0 and 1023: ");
+    delay(100);
+    //Serial.println(enteredNumber);
   }
 
+  
+  while (Serial.available() == 0);
+  enteredNumber = Serial.readStringUntil('\n');
+  enteredNumberInt = enteredNumber.toInt();
+    
+    
+  if ((readValue - enteredNumberInt  <= 50) && (readValue - enteredNumberInt  >= -50))
+  {
+      digitalWrite(ledGreen, HIGH);
+      Serial.println("Great, You won!");
+      delay(500);
+      digitalWrite(ledGreen, LOW);
+      startAgain = true;
+   }
+   
+   else
+   {
+      if (tries > 0)
+      {
+        startAgain = false;
+        digitalWrite(ledYellow, HIGH);
+        delay(500);
+        Serial.println("You have another chance, enter the number:");
+        tries--;
+        digitalWrite(ledYellow, LOW);
+        while(Serial.available() == 0);
+      }
+      
+      else
+      {
+        digitalWrite(ledRed, HIGH);
+        delay(500);
+        Serial.println("You lost, press the button to try again.");
+        digitalWrite(ledRed, LOW);
+      }
+    }
+  delay(25);
 }
