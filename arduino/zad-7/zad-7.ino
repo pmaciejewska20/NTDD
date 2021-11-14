2a1b40c9f58fcf728f81091cb7365834eda67877
Paulina <pmaciejewska20@gmail.com>
2021-11-14 16:45:22 +0100

    LCD - ADC value

--- arduino/zad-7/zad-7.ino
+++ arduino/zad-7/zad-7.ino
@@ -1,21 +1,23 @@
-//LCD (on/off)
+//LCD as a voltometer
 #include <LiquidCrystal.h> // adding library
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 void setup() 
 {
  lcd.begin (16,2);              //type declaration
+ lcd.clear();
  lcd.setCursor(0,0);            //cursor setting
- lcd.print("Welcome");     //Text display
+ lcd.print("read V value:");     //Text display
  lcd.setCursor(0,1);            //cursor setting
- lcd.print("On FORBOT!");     // Text display
- lcd.blink();
+ lcd.print("0.00V");     // Text display
 }
+
+double read;
 
 void loop() 
 {
-   lcd.display();
-   delay(1000);
-   lcd.noDisplay();
-   delay(1000);
+  read = analogRead(A0)*5.0/1024.0;
+  lcd.setCursor(0,1);
+  lcd.print(read);
+  delay(250);
    
 }
