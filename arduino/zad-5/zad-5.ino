d5dfac7823f95635b9b64b4a9e98e680fe0aa737
Paulina <pmaciejewska20@gmail.com>
2021-11-14 16:28:59 +0100

    PWM - gradual LED lightening

--- arduino/zad-5/zad-5.ino
+++ arduino/zad-5/zad-5.ino
@@ -1,5 +1,8 @@
-//PWM 
+//PWM - gradual LED lightening
 #define ledRed 10
+
+int wypelnienie = 0;
+int change = 5;
 void setup() 
 {
   pinMode(ledRed, OUTPUT);
@@ -7,8 +10,14 @@ void setup()
 
 void loop() 
 {
- digitalWrite(ledRed, HIGH);
- delay(1667);
- digitalWrite(ledRed, LOW);
- delay(333);
+  analogWrite(ledRed, wypelnienie);
+  if (wypelnienie < 255)
+  {
+    wypelnienie = wypelnienie + change;
+  }
+  else
+  {
+    wypelnienie = 0;
+  }
+  delay(50);
 }
